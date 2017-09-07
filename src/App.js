6b43f94b72be21824be4819b2275ef75a1db2a02
Peter Wang <happypeter1983@gmail.com>
2017-09-07 11:25:31 +0800

    i

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import dog from './dog.jpg'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <h2>小鸡快跑</h2>
+          <img className='dog' src={dog} alt='dog' />
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
